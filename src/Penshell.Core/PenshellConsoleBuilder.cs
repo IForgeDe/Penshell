eec76643996091d96067a908d3ac3428297be62d
Frank Weller <github@iforge.de>
2019-09-20 21:15:36 +0200

    Api docs aded

--- src/Penshell.Core/PenshellConsoleBuilder.cs
+++ src/Penshell.Core/PenshellConsoleBuilder.cs
@@ -2,8 +2,15 @@ namespace Penshell.Core
 {
     using CliFx.Services;
 
+    /// <summary>
+    /// A fluent builder for building the penshell console.
+    /// </summary>
     public static class PenshellConsoleBuilder
     {
+        /// <summary>
+        /// Builds the <see cref="IConsole"/> instance.
+        /// </summary>
+        /// <returns>The <see cref="IConsole"/> instance.</returns>
         public static IConsole Build()
         {
             return new PenshellConsole();
