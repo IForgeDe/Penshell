bb1e926f09587155958d007dce6d62cbfa095ac5
Frank Weller <github@iforge.de>
2019-09-20 20:42:47 +0200

    Api docs added

--- src/Penshell.Core/IPenshellCLIAdapter.cs
+++ src/Penshell.Core/IPenshellCLIAdapter.cs
@@ -2,10 +2,31 @@ namespace Penshell.Core
 {
     using Microsoft.Extensions.DependencyInjection;
 
+    /// <summary>
+    /// This interface defines the possible and optional methods to adapt an assembly to a penshell command line interface.
+    /// </summary>
     public interface IPenshellCLIAdapter
     {
+        /// <summary>
+        /// The method to configure the di container services.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// services.AddTransient{ExampleCommand}();
+        /// </code>
+        /// </example>
+        /// <param name="services">The service instance, where the injections can be parametrized.</param>
         void ConfigureServices(ServiceCollection services);
 
+        /// <summary>
+        /// This optionally method is called via mef to allow registering of specific command option value converters.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// registry.Add(new ExampleConverter());
+        /// </code>
+        /// </example>
+        /// <param name="registry">The registry instance.</param>
         void RegisterCommandOptionValueConverters(PenshellCommandOptionValueConverterDictionary registry)
         {
         }
