e4ea13fe140e2e91b6a1bc00b5ed52d8ae0bd7f9
Frank Weller <github@iforge.de>
2019-11-09 19:41:38 +0100

    Process Sleep Command added

---
+++ src/Penshell.Commands.Process.Tests/SleepCommandTests.cs
@@ -0,0 +1,48 @@
+﻿namespace Penshell.Commands.Process.Tests
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Test class for <see cref="SleepCommand"/>.
+    /// </summary>
+    [TestFixture(TestOf = typeof(SleepCommand))]
+    public class SleepCommandTests
+    {
+        /// <summary>
+        /// Test for ExecuteAsync with valid values expects valid value.
+        /// </summary>
+        /// <param name="milliseconds">The inital millisecond argument.</param>
+        /// <param name="seconds">The inital seconds argument.</param>
+        /// <param name="minutes">The inital minutes argument.</param>
+        /// <param name="hours">The inital hours argument.</param>
+        /// <param name="expectedMilliseconds">The expected millisecond result.</param>
+        [Test]
+        [TestCase(0, 0, 0, 0, 0d)]
+        [TestCase(1, 0, 0, 0, 1d)]
+        [TestCase(1, 1, 0, 0, 1001d)]
+        [TestCase(1, 1, 1, 0, 61001d)]
+        [TestCase(1, 1, 1, 1, 3661001d)]
+        public void CalculateTimeSpanFromArguments_WithValidValues_ExpectsValidValue(
+            int milliseconds,
+            int seconds,
+            int minutes,
+            int hours,
+            double expectedMilliseconds)
+        {
+            // Arrange
+            var command = new SleepCommand()
+            {
+                Milliseconds = milliseconds,
+                Seconds = seconds,
+                Minutes = minutes,
+                Hours = hours,
+            };
+
+            // Act
+            var timeSpan = command.CalculateTimeSpanFromArguments();
+
+            // Assert
+            Assert.That(timeSpan.TotalMilliseconds, Is.EqualTo(expectedMilliseconds));
+        }
+    }
+}
