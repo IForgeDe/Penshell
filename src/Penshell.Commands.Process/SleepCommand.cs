32bad9d6d008479fd662c2a5010288fe7fbb6e36
Frank Weller <34793594+IForgeDe@users.noreply.github.com>
2019-11-09 19:43:30 +0100

    Process Sleep Command added (#5)

--- src/Penshell.Commands.Process/SleepCommand.cs
+++ src/Penshell.Commands.Process/SleepCommand.cs
@@ -1,5 +1,6 @@
 namespace Penshell.Commands.Process
 {
+    using System;
     using System.Threading;
     using System.Threading.Tasks;
     using CliFx;
@@ -14,24 +15,87 @@ namespace Penshell.Commands.Process
     public class SleepCommand : ICommand
     {
         /// <summary>
+        /// Gets or sets the time in hours that the process should sleep.
+        /// </summary>
+        /// <value>
+        /// The time in hours that the process should sleep.
+        /// </value>
+        [CommandOption("hours", 'H', IsRequired = false, Description = "The time in hours that the process should sleep.")]
+        public int? Hours { get; set; }
+
+        /// <summary>
         /// Gets or sets the time in milliseconds that the process should sleep.
         /// </summary>
         /// <value>
         /// The time in milliseconds that the process should sleep.
         /// </value>
-        [CommandOption("milliseconds", 'm', IsRequired = true, Description = "The time in milliseconds that the process should sleep.")]
+        [CommandOption("milliseconds", 'z', IsRequired = false, Description = "The time in milliseconds that the process should sleep.")]
         public int? Milliseconds { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time in minutes that the process should sleep.
+        /// </summary>
+        /// <value>
+        /// The time in minutes that the process should sleep.
+        /// </value>
+        [CommandOption("minutes", 'm', IsRequired = false, Description = "The time in minutes that the process should sleep.")]
+        public int? Minutes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time in seconds that the process should sleep.
+        /// </summary>
+        /// <value>
+        /// The time in seconds that the process should sleep.
+        /// </value>
+        [CommandOption("seconds", 's', IsRequired = false, Description = "The time in seconds that the process should sleep.")]
+        public int? Seconds { get; set; }
 
         /// <inheritdoc />
         public Task ExecuteAsync(IConsole console)
         {
             console = Guard.Argument(console).NotNull().Value;
-            this.Milliseconds = Guard.Argument(this.Milliseconds).NotNull().NotNegative().Value;
 
-            Thread.Sleep(this.Milliseconds.Value);
+            var delay = CalculateTimeSpanFromArguments();
+
+            delay = Guard.Argument(delay)
+                .NotNegative()
+                .Value;
+
+            Thread.Sleep(delay);
 
             console.Output.WriteLine("Success");
             return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Calculates the <see cref="TimeSpan"/> instance from the given arguments.
+        /// </summary>
+        /// <returns>The calculated <see cref="TimeSpan"/> instance from the given arguments.</returns>
+        internal TimeSpan CalculateTimeSpanFromArguments()
+        {
+            var delay = TimeSpan.Zero;
+
+            if (this.Milliseconds != null)
+            {
+                delay = delay.Add(TimeSpan.FromMilliseconds(this.Milliseconds.Value));
+            }
+
+            if (this.Seconds != null)
+            {
+                delay = delay.Add(TimeSpan.FromSeconds(this.Seconds.Value));
+            }
+
+            if (this.Minutes != null)
+            {
+                delay = delay.Add(TimeSpan.FromMinutes(this.Minutes.Value));
+            }
+
+            if (this.Hours != null)
+            {
+                delay = delay.Add(TimeSpan.FromHours(this.Hours.Value));
+            }
+
+            return delay;
         }
     }
 }
